34136d7712a02f42357b085768665e9dd4c4f7c0
Héctor de Anda <30514545+Hectorreto@users.noreply.github.com>
2024-04-26 22:59:34 -0600

    Example player that moves and shoots bullets

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public float bulletSpeed = 10.0f;
+    public GameObject bulletPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // move the player ignoring where it is facing
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        GetComponent<Rigidbody2D>().velocity = movement * speed;
+
+        // rotate to face the mouse
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 playerPos = transform.position;
+        float angle = Mathf.Atan2(mousePos.y - playerPos.y, mousePos.x - playerPos.x) * Mathf.Rad2Deg - 90.0f;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+
+        // shot a bullet when click mouse
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+            bullet.GetComponent<Rigidbody2D>().velocity = transform.up * bulletSpeed;
+
+            // destroy the bullet after 5 seconds
+            Destroy(bullet, 5.0f);
+        }
+    }
+}
