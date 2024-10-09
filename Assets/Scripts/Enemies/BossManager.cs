afcde629ec41ff0f3e6d6ea786bdef5dd7909e7a
DESKTOP-CBTLII3\Bryan <hc-bryan-@hotmail.com>
2024-10-08 20:34:41 -0600

    Now the Enemy Boss can die after receiving 50 of damage

--- Assets/Scripts/Enemies/BossManager.cs
+++ Assets/Scripts/Enemies/BossManager.cs
@@ -4,38 +4,66 @@ using UnityEngine;
 
 public class BossManager : MonoBehaviour
 {
+    #region DamagetoPlayer
     [SerializeField] private int damageToPlayer = 1;
     [SerializeField] private float damageCooldown = 2f;
     private float nextDamageTime;
-    private BoxCollider2D boxCollider2D;
+    #endregion
+    private CircleCollider2D circleCollider2D;
+
+    [SerializeField] private int health = 50;
+
+    //Receive damage
+    private GameManager gameManager;
+    private int damageReceived = 1;
+
+
     private void Start()
     {
-        boxCollider2D = GetComponent<BoxCollider2D>();
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        circleCollider2D = GetComponent<CircleCollider2D>();
     }
 
     private void Update()
     {
-        if (Cooldown())
+        CheckBossHealth();
+        if (IsDamageOnCooldown())
         {
-            boxCollider2D.enabled = true;
+            circleCollider2D.enabled = true;
         }
     }
 
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        switch (collision.gameObject.tag)
+        {
+            case "Bullet":
+                TakeDamage(damageReceived);
+                Destroy(collision.gameObject);
+                break;
+
+            case "DashShield":
+                TakeDamage(damageReceived);
+                break;
+
+            case "BubbleShield":
+                TakeDamage(damageReceived);
+                collision.gameObject.SetActive(false);
+                break;
+        }
+
         if (collision.gameObject.CompareTag("Player"))
         {
-            if(Cooldown())
+            if(IsDamageOnCooldown())
             {
                 collision.gameObject.GetComponent<CombatManager>().TakeDamage(damageToPlayer);
                 nextDamageTime = Time.time + damageCooldown;
-                boxCollider2D.enabled = false;
+                circleCollider2D.enabled = false;
             }
         }
     }
 
-    private bool Cooldown()
+    private bool IsDamageOnCooldown()
     {
         if(Time.time > nextDamageTime)
         {
@@ -44,6 +72,21 @@ public class BossManager : MonoBehaviour
         else
         {
             return false;
+        }
+    }
+
+    public void TakeDamage(int damageReceived)
+    {
+        health -= damageReceived;
+        print("Vida del boss: " + health);
+    }
+
+    public void CheckBossHealth()
+    {
+        if(health <= 0)
+        {
+            print("Congratulations you won the game!");
+            Destroy(this.gameObject);
         }
     }
 }
